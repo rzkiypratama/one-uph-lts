94aecb464a75ba96b12b85a2444dda90d5d4e69b
rzkiypratama <rzkiypratama>
2024-10-29 08:46:26 +0800

    change e-code functionality button to static

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -226,7 +226,10 @@
 							<h5 class="review-modal-title">I have an E-code</h5>
 							<p class="review-modal-subtitle m-0">Please enter your E-code</p>
 							<input type="text" class="form-control e-code-input my-3 text-center" id="ecodeInput" placeholder="--- --- ---" maxlength="15" />
-							<button type="button" id="submitEcodeBtn" class="btn btn-primary w-100 rounded-0 mt-3" disabled>Submit E-code</button>
+							{{-- uncomment code bellow to use static script e-code for testing purpose, script dir is on public - js - dashboard.js --}}
+							{{-- <button type="button" id="submitEcodeBtn" class="btn btn-primary w-100 rounded-0 mt-3" disabled>Submit E-code</button>
+							 --}}
+							<button type="button" data-bs-toggle="modal" data-bs-target="#eCodeValidModal" class="btn btn-primary w-100 rounded-0 mt-3">Submit E-code</button>
 							<p id="successMessage" class="success-message">E-code is valid! Proceeding...</p>
 							<p id="errorMessage" class="error-message">Invalid E-code! Please try again.</p>
 						</div>
