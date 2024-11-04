6779134128a56cf0ce53c394fd4ce00fbfb63a5c
rzkiypratama <rzkiypratama>
2024-11-04 16:57:26 +0800

    feat: display e-code on frontend

--- resources/views/dashboard/reviewpayment.blade.php
+++ resources/views/dashboard/reviewpayment.blade.php
@@ -199,6 +199,11 @@
 											<p class="payment-title m-0">Entry Option</p>
 											<p class="payment-subtitle m-0">Regular</p>
 										</div>
+
+										<div class="d-flex justify-content-between text-capitalize">
+											<p class="payment-title m-0">E-Code</p>
+											<p class="payment-subtitle m-0">758378399</p>
+										</div>
 									</div>
 
 									<!-- Application Card -->
@@ -231,7 +236,7 @@
 											</div>
 										</div>
 										<div class="d-flex align-items-center justify-content-end w-100 gap-2 pt-2">
-											<label for="installmentTuition" class="form-label mb-0">Payment Type</label>
+											<label for="installmentTuition" class="form-label mb-0">Payment Method</label>
 											<select id="installmentTuition" class="form-select w-50">
 												<option value="" selected hidden>Select Method</option>
 												<option value="installment">Installment</option>
@@ -244,14 +249,14 @@
 						</div>
 
 						<div class="d-flex justify-content-between align-content-center mb-4 gap-3">
-							<button type="button" class="btn btn-outline-danger d-flex align-items-center float-end rounded-0 px-md-5 mt-5 px-4" onclick="window.location.href = '/dashboard/entryoption';">
+							<button type="button" class="btn btn-outline-danger d-flex align-items-center float-end rounded-0 px-md-5 mt-2 px-4" onclick="window.location.href = '/dashboard/entryoption';">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
 									<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" stroke="#FF5351" />
 								</svg>
 								Back
 							</button>
 							<!-- Submit Button Without E-code-->
-							<button id="proceedToPaymentBtn" type="submit" class="btn btn-danger float-end rounded-0 px-md-5 danger-button mt-5" data-bs-toggle="modal" data-bs-target="#firstPaymentModal">
+							<button id="proceedToPaymentBtn" type="submit" class="btn btn-danger float-end rounded-0 px-md-5 danger-button mt-2" data-bs-toggle="modal" data-bs-target="#firstPaymentModal">
 								Proceed to Payment
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
 									<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" fill="#FFF" />
