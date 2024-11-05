cfdd58ac3d60be5856a83c7b1f8ff4b55fe0f148
rzkiypratama <rzkiypratama>
2024-11-05 15:45:48 +0800

    refactor: merge desktop and responsive

--- resources/views/nationality.blade.php
+++ resources/views/nationality.blade.php
@@ -10,8 +10,8 @@
 
 		<div class="d-flex p-md-5 justify-content-md-center align-items-md-center homepage-container px-2 py-4">
 			<!-- desktop view start -->
-			<div class="card card-container d-none d-md-block p-5">
-				<div class="d-flex justify-content-between box-item">
+			<div class="card card-container d-flex justify-content-between p-md-5 p-3">
+				<div class="d-md-flex justify-content-between box-item">
 					<div class="left-content">
 						<p class="text-uppercase">step 1 of 8</p>
 						<h3>Are you Indonesian or International Student?</h3>
@@ -19,9 +19,9 @@
 						<hr class="solid" />
 					</div>
 
-					<div class="d-flex card-box gap-3">
+					<div class="d-flex flex-column flex-md-row card-box gap-3">
 						<a href="/studylevel" class="nationality" data-nationality="National">
-							<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/student.jpeg'); height: 402px; width: 272px">
+							<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/student.jpeg');">
 								<!-- Gradient Overlay -->
 								<div class="gradient-overlay" style="background-image: linear-gradient(to top, #bcd1ff, #f1f1f122)" \></div>
 
@@ -34,7 +34,7 @@
 						</a>
 
 						<a href="/international" class="nationality" data-nationality="International">
-							<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/international-student.png'); height: 402px; width: 272px">
+							<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/international-student.png');">
 								<!-- Gradient Overlay -->
 								<div class="gradient-overlay" style="background-image: linear-gradient(to top, #bcd1ff, #f1f1f122)" \></div>
 
@@ -49,66 +49,16 @@
 				</div>
 
 				<span id="backToSignIn" class="d-flex align-items-md-center btn-sign cursor-pointer gap-2" role="button">
-					<img src="/assets/left-arrow.png" alt="arrow-left" />
-					<p class="back-button p-0">back to Sign In</p>
+					<div class="d-none d-md-block">
+						<img src="/assets/left-arrow.png" alt="arrow-left" />
+						<p class="back-button p-0">back to Sign In</p>
+					</div>
+
+					<button type="button" class="d-md-none btn btn-back-responsive danger-button d-flex justify-content-center align-items-center mt-5 gap-2 px-5"><img src="/assets/arrow-back-red.png"
+							alt="arrow-left" />back to Sign In</button>
 				</span>
 			</div>
 			<!-- desktop view ends -->
-
-			<!-- mobile view start -->
-			<div class="d-flex flex-column d-md-none">
-				<div class="upper-content d-flex align-items-center gap-3">
-					<span class="float-end">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-chevron-left" viewBox="0 0 16 16">
-							<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
-						</svg>
-					</span>
-
-					<span class="text-white" onclick="window.location.href = '/'">
-						<p class="mobile-title">Step 1 of 8</p>
-						<p class="mobile-subtitle">Nationality</p>
-					</span>
-				</div>
-
-				<div class="card rounded-4 middle-content p-3">
-					<div class="left-content">
-						<h3>Are you Indonesian or International Student?</h3>
-						<p class="children">Choose your nationality</p>
-						<hr class="solid" />
-					</div>
-
-					<div class="bagian-bawah d-flex flex-column justify-content-between h-100">
-						<div class="d-flex flex-column card-box gap-3">
-							<a href="/studylevel" class="nationality" data-nationality="National">
-								<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/student.jpeg'); height: 180px; width: auto; background-position: top">
-									<!-- Gradient Overlay -->
-									<div class="gradient-overlay" style="background-image: linear-gradient(to top, #bcd1ff, #f1f1f122)" \></div>
-
-									<!-- Title -->
-									<div class="title-container">
-										<!-- <img src="icon-url.png" alt="icon" class="icon"/> -->
-										<h3 class="title" style="color: #333333">I’m an Indonesian Student</h3>
-									</div>
-								</div>
-							</a>
-
-							<a href="/international" class="nationality" data-nationality="International">
-								<div class="card-nationality fade-in-right border-0" style="background-image: url('/assets/international-student.png'); height: 180px; width: auto; background-position: top">
-									<!-- Gradient Overlay -->
-									<div class="gradient-overlay" style="background-image: linear-gradient(to top, #bcd1ff, #f1f1f122)" \></div>
-
-									<!-- Title -->
-									<div class="title-container">
-										<!-- <img src="icon-url.png" alt="icon" class="icon"/> -->
-										<h3 class="title" style="color: #333333">I’m an International Student</h3>
-									</div>
-								</div>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- mobile view ends -->
 		</div>
 
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
