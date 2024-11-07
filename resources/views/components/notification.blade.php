bb00532ea3b02e3189b06718c302d8dc24f25ffb
rzkiypratama <rzkiypratama>
2024-11-07 10:33:44 +0800

    feat: add ID | EN button on header

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,25 +1,41 @@
-		<div class="d-flex justify-content-between align-items-center mb-md-5 mb-3 px-3 pt-3">
+<div class="d-flex justify-content-between align-items-center mb-md-5 mb-3 pt-3">
+	<div class="d-flex gap-3">
 		<img src="/assets/uph-logo.png" alt="logo" class="uphlogo" />
-		<div class="d-flex align-items-center gap-3">
 		<span class="logout-button d-flex align-items-center gap-2">
-		<p class="d-md-block d-none m-0">dashboard</p>
-		<img src="/assets/signout-icon.png" alt="signout icon" class="logout-icon d-block d-md-none" />
+			<p class="d-md-block d-none m-0" onclick="window.location.href = '/dashboard'">dashboard</p>
 		</span>
+	</div>
+	<div class="d-flex align-items-center gap-3">
+
+		<span class="logout-button d-flex align-items-center gap-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+				<path
+					d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+			</svg>
+			<p class="d-md-block d-none m-0">my profile</p>
+		</span>
+
+		<span class="language-button">
+			<p>ID</p>
+			<span>|</span>
+			<p class="active">EN</p>
+		</span>
+
 		<img src="/assets/bells.png" alt="notification icon" class="notification-icon" id="notificationIcon" />
 
 		<!-- Notification Card -->
 		<div class="notification-card" id="notificationCard">
-		<p>New Message: Welcome to the system!</p>
-		<p>Reminder: Complete your profile.</p>
-		<p>Reminder: Complete your profile.</p>
-		<p>Reminder: Complete your profile.</p>
-		<p>New Update Available</p>
-		<p>New Update Available</p>
-		<p>New Update Available</p>
-		<p>New Update Available</p>
-		<p>New Update Available</p>
+			<p>New Message: Welcome to the system!</p>
+			<p>Reminder: Complete your profile.</p>
+			<p>Reminder: Complete your profile.</p>
+			<p>Reminder: Complete your profile.</p>
+			<p>New Update Available</p>
+			<p>New Update Available</p>
+			<p>New Update Available</p>
+			<p>New Update Available</p>
+			<p>New Update Available</p>
 		</div>
-		</div>
-		</div>
+	</div>
+</div>
 
-		<script type="text/javascript" src="{{ asset('js/notificationIcon.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/notificationIcon.js') }}"></script>
