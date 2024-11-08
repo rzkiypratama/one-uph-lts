41a39b95d494cc48a3dbe099ed5bde8c42ff67ea
rzkiypratama <rzkiypratama>
2024-11-08 11:01:42 +0800

    fix button position

--- resources/views/dashboard/reviewpayment.blade.php
+++ resources/views/dashboard/reviewpayment.blade.php
@@ -245,7 +245,7 @@
 							</div>
 						</div>
 
-						<div class="d-flex justify-content-between align-content-center mb-4 gap-3">
+						<div class="d-flex justify-content-between align-content-center gap-3">
 							<button type="button" class="btn btn-outline-danger d-flex align-items-center float-end rounded-0 px-md-5 mt-2 px-4" onclick="window.location.href = '/dashboard/entryoption';">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
 									<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" stroke="#FF5351" />
@@ -253,7 +253,7 @@
 								Back
 							</button>
 							<!-- Submit Button Without E-code-->
-							<button id="proceedToPaymentBtn" type="submit" class="btn btn-danger float-end rounded-0 px-md-5 danger-button mt-2" data-bs-toggle="modal" data-bs-target="#firstPaymentModal">
+							<button id="proceedToPaymentBtn" type="submit" class="btn btn-danger float-end rounded-0 px-md-5 danger-button" data-bs-toggle="modal" data-bs-target="#firstPaymentModal">
 								Proceed to Payment
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
 									<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" fill="#FFF" />
