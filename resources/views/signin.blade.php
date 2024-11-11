7ad8a6fea9552272b8cb83e1413cfd9f15b56080
rzkiypratama <rzkiypratama>
2024-11-12 07:26:19 +0800

    enhance styling & add forgot password route

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -21,7 +21,7 @@
 			<div class="d-flex justify-content-center align-items-center">
 				<div class="card p-4" style="width: 360px">
 					<div class="d-flex justify-content-between text-center">
-						<h3>Sign In</h3>
+						<p class="login-card-title">Sign In</p>
 						<img src="/assets/signin.png" alt="Sign In Image" class="img-fluid mb-3" style="max-width: 100px" />
 					</div>
 
@@ -31,28 +31,30 @@
 
 					<!-- Email Address Input -->
 					<form id="loginForm">
-						<div class="mb-3">
-							<label for="email" class="form-label">Email Address</label>
-							<input type="email" class="form-control" id="email" placeholder="Enter your email" required />
-						</div>
+						<div style="height: 280px">
+							<div class="mb-3">
+								<label for="email" class="form-label">Email Address</label>
+								<input type="email" class="form-control" id="email" placeholder="Enter your email" required />
+							</div>
 
-						<!-- Password Input with Show/Hide Eye Icon -->
-						<div class="position-relative mb-3">
-							<label for="password" class="form-label">Password</label>
-							<input type="password" class="form-control" id="password" placeholder="Enter your password" required />
-							<i class="bi bi-eye-slash eye-icon" id="togglePassword"></i>
-						</div>
+							<!-- Password Input with Show/Hide Eye Icon -->
+							<div class="position-relative mb-3">
+								<label for="password" class="form-label">Password</label>
+								<input type="password" class="form-control" id="password" placeholder="Enter your password" required />
+								<i class="bi bi-eye-slash eye-icon" id="togglePassword"></i>
+							</div>
 
-						<!-- Forgot Password Link -->
-						<div class="mb-3 text-end">
-							<a href="#" class="forgot-password fst-italic">Forgot Password?</a>
-						</div>
+							<!-- Forgot Password Link -->
+							<div class="mb-3 text-end">
+								<a href="/forgot-password" class="forgot-password fst-italic">Forgot Password?</a>
+							</div>
 
-						<!-- Sign In Button -->
-						<div class="d-grid mb-3">
-							<button type="submit" class="btn btn-signin">Sign In</button>
+							<!-- Sign In Button -->
+							<div class="d-grid mb-3">
+								<button type="submit" class="btn btn-signin">Sign In</button>
+							</div>
+							<p class="register text-center">Don’t have an account yet? <a href="/nationality" class="register-link">Register</a></p>
 						</div>
-						<p class="register text-center">Don’t have an account yet? <a href="/nationality" class="register-link">Register</a></p>
 					</form>
 				</div>
 			</div>
