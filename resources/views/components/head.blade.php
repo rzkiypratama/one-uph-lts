f6515e962f4ec413d82bc12beb821fe3169661d1
rzkiypratama <rzkiypratama>
2024-11-29 16:40:09 +0800

    feat: add contact us to entire page

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,6 +2,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://kit.fontawesome.com/49c31576f5.js" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}" />
@@ -10,3 +11,5 @@
 	<!-- Inline CSS slot -->
 	{{ $inlineCSS ?? '' }}
 </head>
+
+<x-contactus />
