44ead89b4a02c6e0a56852154307689a8ecf4ff1
rzkiypratama <rzkiypratama>
2024-12-05 16:59:41 +0800

    feat: add offering letter button

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -57,7 +57,7 @@
 			<h3 class="dashboard-username">Kevin Saputra Siahaan</h3>
 			<span class="d-flex justify-content-between align-items-center">
 				<p class="text-uppercase dashboard-id">ID #123456</p>
-				<button type="button" class="btn btn-outline-primary dashboard-edit d-flex align-items-center gap-2 px-4"><img src="/assets/download-icon.png" alt="download-icon" /> Offering Letter</button>
+				<button type="button" class="btn btn-outline-primary dashboard-edit">offering letter</button>
 			</span>
 		</div>
 		<!-- mobile layout -->
@@ -72,13 +72,7 @@
 						<h3 class="dashboard-username">Kevin Saputra Siahaan</h3>
 						<span class="d-flex justify-content-between align-items-end">
 							<p class="text-uppercase dashboard-id">ID #123456</p>
-							<button type="button" class="btn btn-outline-primary dashboard-edit px-4">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-									<path
-										d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-								</svg>
-								edit profile
-							</button>
+							<button type="button" class="btn btn-outline-primary dashboard-edit d-flex align-items-center gap-2 px-4"><img src="/assets/download-icon.png" alt="download-icon" /> Offering Letter</button>
 						</span>
 						<hr class="full-width-hr-wrapper-form" />
 
--- resources/views/tuition-fee/dashboard.blade.php
+++ resources/views/tuition-fee/dashboard.blade.php
@@ -27,6 +27,8 @@
 						<h3 class="dashboard-username">Kevin Saputra Siahaan</h3>
 						<span class="d-flex justify-content-between align-items-center">
 							<p class="text-uppercase dashboard-id">ID #123456</p>
+							<button type="button" class="btn btn-outline-primary dashboard-edit d-flex align-items-center gap-2 px-4"><img src="/assets/download-icon.png" alt="download-icon" /> Offering Letter</button>
+
 						</span>
 					</div>
 
