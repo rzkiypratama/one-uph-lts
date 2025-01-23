af8135157ac8197392f6e01fbdb4647cd4cfd829
rzkiypratama <rzkiypratama>
2025-01-23 16:59:08 +0800

    feat: add favicon

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,10 +3,11 @@
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://kit.fontawesome.com/49c31576f5.js" crossorigin="anonymous"></script>
+	<link rel="icon" href="{{ url('assets/favicon.ico') }}">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-	<title>{{ $title ?? 'Default Title' }}</title>
+	<title>UPH | Online Admission</title>
 
 	<!-- Inline CSS slot -->
 	{{ $inlineCSS ?? '' }}
